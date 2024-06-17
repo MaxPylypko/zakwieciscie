6a526aba6e335e66e77507f38c46351b391dcab7
MaxPylypko <f1cuscay@gmail.com>
2024-06-17 13:41:50 +0300

    [main, contacts] main, contacts pages added

--- contacts/views.py
+++ contacts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def contacts(request):
-    return render(request, "contacts/contacts.html")
+    return render(request, 'contacts/contacts.html')
